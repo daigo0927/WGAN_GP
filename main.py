d85b330642731f4336d83d44da948c26d11b51ff
Daigo HIROOKA <tappy.hello-planet@ezweb.ne.jp>
2017-06-27 15:17:02 +0900

    add gradient penalty

--- main.py
+++ main.py
@@ -2,18 +2,12 @@
 
 import os, sys
 import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
-import glob
 import pdb
 from PIL import Image
 import h5py
 import argparse
 from tqdm import tqdm
 
-from keras.models import Sequential, Model
-from keras.optimizers import RMSprop
-from keras.layers import Input
 import keras.backend as K
 import tensorflow as tf
 
@@ -71,8 +65,8 @@ def main():
 
     wgan = WassersteinGAN(gen = gen, disc = disc,
                           z_dim = 100, image_size = args.image_size,
-                          lr_d = 5e-5, # args.lr_d,
-                          lr_g = 1e-5) # args.lr_g)
+                          lr_d =  args.lr_d,
+                          lr_g =  args.lr_g)
 
     sampler = InputSampler(datadir = args.datadir,
                            target_size = args.target_size, image_size = args.image_size,
@@ -98,9 +92,12 @@ class WassersteinGAN:
         self.z_dim = z_dim
         self.image_size = image_size
 
-        self.x = tf.placeholder(tf.float32, (None, self.image_size, self.image_size, 3),
+        self.x = tf.placeholder(tf.float32,
+                                (None, self.image_size, self.image_size, 3),
                                 name = 'x')
-        self.z = tf.placeholder(tf.float32, (None, self.z_dim), name = 'z')
+        self.z = tf.placeholder(tf.float32,
+                                (None, self.z_dim),
+                                name = 'z')
         self.x_ = self.gen(self.z)
 
         self.d = self.disc(self.x)
@@ -109,17 +106,26 @@ class WassersteinGAN:
         self.d_loss = -(tf.reduce_mean(self.d) - tf.reduce_mean(self.d_))
         self.g_loss = -tf.reduce_mean(self.d_)
 
-        self.lr_d = tf.placeholder(tf.float32, shape = ())
-        self.lr_d_schedule = np.array([lr_d, lr_d/5, lr_d/10, lr_d/50])
-        self.lr_g = tf.placeholder(tf.float32, shape = ())
-        self.lr_g_schedule = np.array([lr_g, lr_g/5, lr_g/10, lr_g/50])
+        # gradient penalty
+        self.alpha = tf.random_uniform((tf.shape(self.x)[0], 1),
+                                       minval = 0., maxval = 1,)
+        self.differ = self.x_ - self.x
+        self.interp = self.x + (self.alpha * self.differ)
+        self.grads = tf.gradients(self.disc(self.interp), [self.interp])[0]
+        self.slopes = tf.sqrt(tf.reduce_sum(tf.square(self.grads),
+                                            reduction_indices = [3]))
+        self.grad_penalty = tf.reduce_mean((self.slopes - 1.)**2)
+        self.d_loss += 10 * self.grad_penalty
 
-        self.d_opt = tf.train.RMSPropOptimizer(learning_rate = self.lr_d)\
+        self.lr_d = lr_d
+        self.lr_g = lr_g
+
+        self.d_opt = tf.train.AdamOptimizer(learning_rate = self.lr_d,
+                                            beta1 = 0., beta2 = 0.9)\
                      .minimize(self.d_loss, var_list = self.disc.trainable_weights)
-        self.g_opt = tf.train.RMSPropOptimizer(learning_rate = self.lr_g)\
+        self.g_opt = tf.train.AdamOptimizer(learning_rate = self.lr_g,
+                                            beta1 = 0., beta2 = 0.9)\
                      .minimize(self.g_loss, var_list = self.gen.trainable_weights)
-
-        self.saver = tf.train.Saver()
 
         self.sess = tf.Session()
         K.set_session(self.sess)
@@ -136,18 +142,6 @@ class WassersteinGAN:
 
         # training iteration
         for e in range(epochs):
-            if e < 4:
-                lr_d = self.lr_d_schedule[0]
-                lr_g = self.lr_g_schedule[0]
-            elif e >= 4 and e < 8:
-                lr_d = self.lr_d_schedule[1]
-                lr_g = self.lr_g_schedule[1]
-            elif e >= 8 and e < 15:
-                lr_d = self.lr_d_schedule[2]
-                lr_g = self.lr_g_schedule[2]
-            else:
-                lr_d = self.lr_d_schedule[3]
-                lr_g = self.lr_g_schedule[3]
                 
             for batch in range(num_batches):
 
@@ -165,12 +159,10 @@ class WassersteinGAN:
                     bx = sampler.image_sample(batch_size)
                     bz = sampler.noise_sample(batch_size)
                     self.sess.run(self.d_opt, feed_dict = {self.x: bx, self.z: bz,
-                                                           self.lr_d: lr_d,
                                                            K.learning_phase(): 1})
 
                 bz = sampler.noise_sample(batch_size, self.z_dim)
                 self.sess.run(self.g_opt, feed_dict = {self.z: bz,
-                                                       self.lr_g: lr_g,
                                                        K.learning_phase(): 1})
 
                 if batch%10 == 0:
@@ -193,125 +185,3 @@ class WassersteinGAN:
 
 if __name__ == '__main__':
     main()
-        
-        
-def train():
-    sess = tf.Session()
-
-    if args.generator == 'deconv':
-        gen = generator_deconv(image_size = args.image_size)
-    elif args.generator == 'upsampling':
-        gen = generator_upsampling(image_size = args.image_size)
-    disc = discriminator(image_size = args.image_size)
-
-    # feed setup
-    z_in = tf.placeholder(tf.float32, shape=[None, 100])
-    image_real = tf.placeholder(tf.float32,
-                                shape = [None,
-                                         args.image_size, args.image_size, 3])
-    image_fake = gen(z_in)
-    pred_real = disc(image_real)
-    pred_fake = disc(image_fake)
-    loss_g = -K.mean(pred_fake)
-    loss_d = -(K.mean(pred_real) - K.mean(pred_fake))
-    '''
-    eps = K.random_uniform(shape = [K.shape(z_in)[0],1,1,1])
-    image_inter = image_true - eps*(image_true - image_fake)
-    grad = K.gradients(disc(image_inter), [image_inter])[0]
-    gradpenalty = K.mean(K.square(\
-                                  K.sqrt(K.sum(K.square(grad), axis = 1))\
-                                  - 1))
-    loss_d = loss_d + 10*gradpenalty
-    '''
-
-    # set optimizer
-    '''
-    d_opt = tf.train.AdamOptimizer(learning_rate = args.lr_d, beta1 = 0.5, beta2 = 0.9)\
-            .minimize(loss_d, var_list = disc.trainable_weights)
-    g_opt = tf.train.AdamOptimizer(learning_rate = args.lr_g, beta1 = 0.5, beta2 = 0.9)\
-            .minimize(loss_g, var_list = gen.trainable_weights)
-    '''
-    d_opt = tf.train.RMSPropOptimizer(learning_rate = 5e-5)\
-                    .minimize(loss_d, var_list = disc.trainable_weights)
-    for w_d in disc.trainable_weights:
-        print(w_d)
-    g_opt = tf.train.RMSPropOptimizer(learning_rate = 5e-5)\
-                    .minimize(loss_g, var_list = gen.trainable_weights)
-    for w_g in gen.trainable_weights:
-        print(w_g)
-    
-    sess.run(tf.global_variables_initializer())
-
-    # load weight (if needed)
-    if not args.loadweight == False:
-        disc.load_weights(filepath = args.loadweight+'/wgan_d.h5',
-                          by_name = False)
-        gen.load_weights(filepath = args.loadweight+'/wgan_g.h5',
-                         by_name = False)
-
-    # data(image) path list
-    paths = []
-    for ddir in args.datadir:
-        paths = paths + glob.glob(ddir + '/*')
-    datasize = min(len(paths), args.train_size)
-    print('data size : {}'.format(datasize))
-    paths = np.random.choice(paths, datasize, replace = False)
-
-    # trainig schedule
-    epochs = args.epochs
-    batch_size = args.batch_size
-    num_batches = int(len(paths)/batch_size)
-    print('Number of batches : {}, epochs : {}'.format(num_batches, epochs))
-
-    # training
-    for epoch in range(epochs):
-        
-        for batch in range(num_batches):
-            
-            # load data splitingly
-            if batch in np.linspace(0, num_batches, args.splitload+1, dtype = int):
-                path_split = np.random.choice(paths,
-                                              int(len(paths)/args.splitload),
-                                              replace = False)
-                data = np.array([get_image(p,
-                                           args.image_target,
-                                           args.image_size)\
-                                 for p in tqdm(path_split)])
-
-            # train discriminator
-            if epoch == 0 and batch < 20:
-                nd = 100
-            elif batch%500 == 0:
-                nd = 100
-            else:
-                nd = args.nd
-            for _ in range(nd):
-                d_weights = [np.clip(w, -0.01, 0.01) for w in disc.get_weights()]
-                disc.set_weights(d_weights)
-                # true image
-                x_real = data[np.random.choice(len(data),
-                                               batch_size,
-                                               replace = False)]
-                # fake seed
-                z = np.random.uniform(-1, 1, (batch_size, 100))
-                feeder = {z_in: z, image_real: x_real, K.learning_phase(): 0}
-                sess.run(d_opt, feeder)
-
-            # train generator
-            z = np.random.uniform(-1, 1, (batch_size, 100))
-            sess.run(g_opt, {z_in: z, K.learning_phase(): 0})
-
-            print('epoch:{}, batch:{}, g_loss:{}, d_loss:{}'\
-                  .format(epoch, batch,
-                          *sess.run([loss_g, loss_d], feeder)))
-
-            if batch%100 == 0:
-                x_fake = sess.run(image_fake, {z_in: z, K.learning_phase(): 0})
-                sample = combine_images(x_fake)
-                sample = sample*127.5 + 127.5
-
-                Image.fromarray(sample.astype(np.uint8))\
-                     .save(args.sampledir + '/sample_{}_{}.png'.format(epoch, batch))
-
-        gen.save_weights(args.weightdir + '/wgan_g_{}epoch.h5'.format(epoch))
-        disc.save_weights(args.weightdir + '/wgan_d_{}epoch.h5'.format(epoch))
